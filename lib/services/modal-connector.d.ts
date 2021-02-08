f30abd03d3652e8a6a205de3227efd80d071d536
Kuba Fogel <kuba.fogel@movecloser.pl>
2021-02-08 10:28:32 +0100

    Upgraded version to rc.39

--- lib/services/modal-connector.d.ts
+++ lib/services/modal-connector.d.ts
@@ -6,41 +6,49 @@ export declare class ModalConnector implements IModal {
     protected _stream$: BehaviorSubject<ModalState>;
     constructor(registry: ModalRegistry<any>);
     /**
-     *
+     * Method to trigger closing of a modal.
      * @param key
      */
     close(key?: string | null): void;
     /**
-     *
+     * Returns current component from state.
      */
     component<C>(): C;
     /**
-     *
+     * Resolve component from registry by it's name.
+     * @param name
+     */
+    getComponent<C>(name: string): C;
+    /**
+     * Returns boolean if modal is currently opened.
      */
     get isOpened(): boolean;
     /**
-     *
+     * Returns name of current modal component.
      */
     get name(): string | null;
     /**
-     *
-     // * @param key
+     * Returns array of registered components.
+     */
+    getRegistry<C>(): ModalRegistry<C>;
+    /**
+     * Method to trigger modal opening.
      * @param payload
      */
     open(key: string, payload?: ModalPayload): void;
     /**
-     *
+     * Method to asynchronously trigger modals opening.
      * @param key
      * @param promise
      * @param payload
      */
     openAsync(key: string, promise: Promise<any>, payload?: ModalPayload): void;
     /**
-     *
+     * Returns payload of currently opened modal.
      */
     get payload(): ModalPayload;
     /**
-     *
+     * Method to subscribe for modals stream.
      * @param callback
      */
     subscribe(callback: (open: ModalState) => any): void;
