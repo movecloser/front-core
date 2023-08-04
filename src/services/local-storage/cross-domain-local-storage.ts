7ec83f1217015eb67a8a82b89e619ae5468201d7
Agnieszka Zawadzka <agnieszka.zawadzka@movecloser.pl>
2023-08-04 12:50:01 +0200

    test requestStorageAccess

--- src/services/local-storage/cross-domain-local-storage.ts
+++ src/services/local-storage/cross-domain-local-storage.ts
@@ -147,7 +147,63 @@ export class CrossDomainLocalStorageProvider implements ILocalStorage {
           default:
             console.log('Unknown method', e.data.method)
         }
-    }`
+    }
+    
+    function doThingsWithCookies() {
+        console.info('Cookie!')
+        document.cookie = "foo=bar"; // set a cookie
+    }
+
+    async function handleCookieAccess() {
+        if (!document.hasStorageAccess) {
+            // This browser doesn't support the Storage Access API
+            // so let's just hope we have access!
+            doThingsWithCookies();
+        } else {
+            const hasAccess = await document.hasStorageAccess();
+            if (hasAccess) {
+                // We have access to unpartitioned cookies, so let's go
+                doThingsWithCookies();
+            } else {
+                // Check whether unpartitioned cookie access has been granted
+                // to another same-site embed
+                try {
+                    const permission = await navigator.permissions.query({
+                        name: "storage-access",
+                    });
+
+                    if (permission.state === "granted") {
+                        // If so, you can just call requestStorageAccess() without a user interaction,
+                        // and it will resolve automatically.
+                        await document.requestStorageAccess();
+                        doThingsWithCookies();
+                    } else if (permission.state === "prompt") {
+                        // Need to call requestStorageAccess() after a user interaction
+                        /*btn.addEventListener("click", async () => {
+                            try {
+                                await document.requestStorageAccess();
+                                doThingsWithCookies();
+                            } catch (err) {
+                                // If there is an error obtaining storage access.
+                                console.error('Error obtaining storage access: $ {err}. Please sign in.');
+                            }
+                        });*/
+                        console.error('Error obtaining storage access: $ {err}. Please sign in.');
+                    } else if (permission.state === "denied") {
+                        // User has denied unpartitioned cookie access, so we'll
+                        // need to do something else
+                        console.error('Storage access denied');
+                    }
+                } catch (error) {
+                    console.log('Could not access permission state. Error: $ {error}');
+                    doThingsWithCookies(); // Again, we'll have to hope we have access!
+                }
+            }
+        }
+    }
+
+    handleCookieAccess()
+    `
 
     const channel = new MessageChannel()
 
